eb5df7b033d5d688732eef23cd7d2efed46e5d28
Rendy Tomaluweng <rendytomaluweng@Rendys-MacBook-Pro.local>
2026-01-18 23:06:01 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Index from "./pages/Index.tsx";
+import Solution from "./pages/Solutions.tsx";
+import About from "./pages/About.tsx";
+import HelpandContact from "./pages/HelpAndContact.tsx";
+
+const App = () => {
+  return <div>Hello World</div>;
+};
+export default App;
